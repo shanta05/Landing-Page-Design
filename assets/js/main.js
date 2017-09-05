6e06422288f26ec2d36be4b3eb4072d5e745e7d6
Shantanu Debnath <sdnath017@gmail.com>
2017-09-06 01:06:53 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,24 @@
 
     			fitWidth: true
             });
+		  $(".single-portfolio-item-27").hover(function(){
+		  	$(".single-portfolio-item-27, .portfolio-hover-27 h2, .portfolio-hover-27 p").removeClass("animated slideInUp");
+		  	$(this).find(".portfolio-hover-27 h2, .portfolio-hover-27 p").addClass("animated slideInUp");
+		  });
+
+		  $(".menu-triger-27").on('click',function(){
+
+		  	$(".off-canvus-menu").addClass("right-zero");
+		  	$(".off-canvus-menu-overlay").addClass("overlay-visible");
+
+		  });
+
+		  $(".menu-close,.off-canvus-menu-overlay").on('click',function(){
+
+		  	$(".off-canvus-menu").removeClass("right-zero");
+		  	$(".off-canvus-menu-overlay").removeClass("overlay-visible");
+
+		  });
 
 
         
